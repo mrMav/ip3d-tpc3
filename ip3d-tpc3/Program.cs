9e03673b1093c68651c170f6e142302c275f89b3
mrmav <mrmav@outlook.pt>
2018-11-22 21:02:12 +0000

    Add project files.

---
+++ ip3d-tpc3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ip3d_tpc3
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
